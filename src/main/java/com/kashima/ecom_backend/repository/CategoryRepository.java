7ac4cbedf9f036341cbaf40daeb59f8e34e3e8d4
Hiren Solanki <hirensolanki9697@gmail.com>
2025-11-28 19:12:16 +0530

    bug: solved Typo errors name mismatch with table column name

--- src/main/java/com/kashima/ecom_backend/repository/CategoryRepository.java
+++ src/main/java/com/kashima/ecom_backend/repository/CategoryRepository.java
@@ -10,9 +10,9 @@ public interface CategoryRepository extends JpaRepository<Category,Long> {
     public Category findByName(String name);
 
     @Query("SELECT c FROM Category c "+
-        "WHERE c.name=:name AND c.parantCategory.name = :parantCategoryName"
+        "WHERE c.name=:name AND c.parentCategory.name = :parentCategoryName"
     )
     public Category findByNameAndParent(@Param("name")  String name,
-                                        @Param("parantCategoryName") String parentCategoryName);
+                                        @Param("parentCategoryName") String parentCategoryName);
 
 }
